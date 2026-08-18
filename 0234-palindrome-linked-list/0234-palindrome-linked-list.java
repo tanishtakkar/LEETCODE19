c8200f48f44b48e0ea439a39397876d1bc3f567f
Tanish Takkar <legeidtanish@gmail.com>
2026-08-18 11:16:43 +0530

    Time: 3 ms (99.83%), Space: 94.2 MB (90.57%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,39 +10,37 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(checkPalindrome(head)==1){
-            return true;
+        if(head==null || head.next ==null){
+        return true;
+       }
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        return false;
+        
+        ListNode prev = null; 
+        ListNode curr=slow.next;
+        while(curr!=null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        } 
+        ListNode first = head;
+        ListNode second = prev;
+        while(second!=null){
+            if(first.val != second.val){
+                return false;
+            }
+            else{
+                first = first.next;
+                second = second.next;
+            }  
+        }
+        return true;
     }
-   private static int checkPalindrome(ListNode head) {
-    if(head==null || head.next==null){
-        return 1;
-    }
-      ListNode slow=head;
-      ListNode fast=head;
-      while(fast.next!=null && fast.next.next!=null){
-          slow=slow.next;
-          fast=fast.next.next;
-      }
-      slow=slow.next;
-      ListNode prev=null;
-      while(slow!=null){
-          ListNode next=slow.next;
-          slow.next=prev;
-          prev=slow;
-          slow=next;
-      }
-      ListNode first=head;
-      ListNode second=prev;
-      while(second!=null){
-          if(first.val!=second.val){
-              return 0;
-          }
-          first=first.next;
-          second=second.next;
-      }
-      return 1;
-  }
 
 }
