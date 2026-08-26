a666cb4e78cab0638b25c0f8cc3f685d6570e8d7
Tanish Takkar <legeidtanish@gmail.com>
2026-08-26 11:56:17 +0530

    Time: 0 ms (100%), Space: 46.7 MB (30.35%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -18,29 +18,39 @@ public class Solution {
             slow=slow.next;
             fast=fast.next.next;
             if(fast==slow){
-                ListNode temp=slow.next;
-                length=1;
-                while(temp!=slow){
-                    temp=temp.next;
-                    length++;
+                // ListNode temp=slow.next;
+                // length=1;
+                // while(temp!=slow){
+                //     temp=temp.next;
+                //     length++;
                     
-                }
+                // }
                 break;
             }
         }
-        if(length==0){
-            return null;
+            if(fast==null || fast.next==null){
+                return null;
+            }
+        
+        // if(length==0){
+        //     return null;
+        // }
+        // ListNode f=head;
+        // ListNode s=head;
+        // while(length>0){
+        //     s=s.next;
+        //     length--;
+        // }
+        // while(f!=s){
+        //     f=f.next;
+        //     s=s.next;
+        // }
+        // return s;
+        slow=head;
+        while(slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
         }
-        ListNode f=head;
-        ListNode s=head;
-        while(length>0){
-            s=s.next;
-            length--;
-        }
-        while(f!=s){
-            f=f.next;
-            s=s.next;
-        }
-        return s;
+        return slow;
     }
 }
